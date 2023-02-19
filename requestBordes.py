43bea7ff03f82541cde33b505c1ef3f589f8ac7e
Eero <eeero93@gmail.com>
2023-02-19 14:17:14 +0200

    Make script more readable

--- requestBordes.py
+++ requestBordes.py
@@ -1,25 +1,29 @@
 import os
 import sys
-import json
 import requests
 import pandas as pd
-import numpy as np
 
-tilarajat = pd.read_csv('./metsaanTilat.csv',
+tunnusDF = pd.read_csv('./metsaanTilat.csv',
                         usecols=[0])
 
-rajaLista = tilarajat['tunnus'].values.tolist()
-rajaLista = sorted(rajaLista)
+tunnusLista = tunnusDF['tunnus'].values.tolist()
+tunnusLista = sorted(tunnusLista)
 
 apiKey = '3ab64aed-4375-4586-bfe7-fe9a7f580c03'
 
-for t in rajaLista:
-    kiTu = t
-    printThis = f'Downloading borders for {kiTu}'
-    print(printThis)
-    stuff = f'PalstanSijaintitiedot/items?kiinteistotunnuksenEsitysmuoto={kiTu}'
-    api = f'https://avoin-paikkatieto.maanmittauslaitos.fi/kiinteisto-avoin/simple-features/v3/collections/{stuff}'
-    filename = f'./rajat/{kiTu}.json'
-    r = requests.get(api, auth=(apiKey, ''))
-    with open(filename, 'wb') as f:
-        f.write(r.content)
+for tunnus in tunnusLista:
+    kiTu = tunnus
+    if tunnus.find('M') != -1:
+        printThis = f'Kiinteistöllä {kiTu} lohkominen tms kesken, skipataan'
+        print(printThis)
+    else:
+        printThis = f'Ladataan kiinteistön {kiTu} rajoja'
+        print(printThis)
+        stuff = f'PalstanSijaintitiedot/items?kiinteistotunnuksenEsitysmuoto={kiTu}'
+        api = f'https://avoin-paikkatieto.maanmittauslaitos.fi/kiinteisto-avoin/simple-features/v3/collections/{stuff}'
+        filename = f'./rajat/{kiTu}.json'
+        r = requests.get(api, auth=(apiKey, ''))
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+print('Valmis')
